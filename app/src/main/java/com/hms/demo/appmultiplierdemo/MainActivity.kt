405d7c68a639bc18fe3169a2be440460a62dd1c2
Daniel Mendoza <dan.ms.0739@gmail.com>
2021-05-06 08:45:24 -0500

    Added the capability to launch in full screen mode and use magic windows once a new activity is opened

--- app/src/main/java/com/hms/demo/appmultiplierdemo/MainActivity.kt
+++ app/src/main/java/com/hms/demo/appmultiplierdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hms.demo.appmultiplierdemo
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -22,15 +23,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         b2.setOnClickListener(this)
         b3.setOnClickListener(this)
 
-        val config: String = getResources().getConfiguration().toString()
+        val config: String = resources.configuration.toString()
         val isInMagicWindow =
             config.contains("hwMultiwindow-magic") || config.contains("hw-magic-windows")
         val message="Magic window: ${if(isInMagicWindow) "true" else "false"}"
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
-        val orientation=getResources().getConfiguration().orientation
+        val orientation= resources.configuration.orientation
 
         //Force to show in full screen
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         //Back to split view
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
@@ -51,6 +52,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 Intent(this, RedActivity::class.java)
             }
         }
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+
         startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isTaskRoot){
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
     }
 }
